158d08d08e3df0475a441a35019812a9f3d05d7e
root <kimchannchon101@gmail.com>
2022-12-26 23:39:36 +0700

    header hero background-animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to my world<br/>
+        Hi, I'm Kimchann Chon
+      </SectionTitle>
+      <SectionText>
+        Freelancing on Web development | Data Science | Digital Marketing
+      </SectionText>
+      <Button onClick={() => window.location = 'https://kimchannchon.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
